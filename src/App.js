218482bc6fade6911ef7e26fc3176b6b89ae8972
Jairvsosa98 <jairvillazonsosa98@outlook.com>
2024-02-26 21:55:57 -0500

    working with tailwindcss cdn

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
-import { getMonth } from './utils/util.js';
-import Sidebar from './components/Sidebar.js';
-import CalendarHeader from './components/CalendarHeader.js';
-import Month from './components/Month.js';
+import React, { useState, useContext, useEffect } from "react";
+import "./App.css";
+import { getMonth } from "./utils/util.js";
+import Sidebar from "./components/Sidebar.js";
+import CalendarHeader from "./components/CalendarHeader.js";
+import Month from "./components/Month.js";
+import GlobalContext from "./context/GlobalContext.js";
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
-
+  const { monthIndex } = useContext(GlobalContext);
+  useEffect(() => {
+    setCurrentMonth(monthIndex);
+  }, [monthIndex]);
   return (
     <React.Fragment>
-      <div className="h-screen flex flex-columns">
+      <div className="h-screen flex flex-col">
         <CalendarHeader />
         <div className="flex flex-1">
           <Sidebar />
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,11 +1,16 @@
 import React from "react";
-
-export default function Day({day}){
+import dayjs from "dayjs";
+export default function Day({ day, rowIdx }) {
+    function getCurrentDayClass() {
+        return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? 'bg-blue-600 text-white rounded-full w-7' : ''
+    }
     return (
         <div className="border boder-gray-200 flex flex-col">
-            <header className="flex flex-col items-center"> 
-            <p className="text-sm mt-1">{day.format('ddd')}</p> 
-            <p className="text-sm p-1 my-1 text-center">{day.format("DD")}</p>
+            <header className="flex flex-col items-center">
+                {rowIdx === 0 && (
+                    <p className="text-sm mt-1">{day.format('ddd')}</p>
+                )}
+                <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>{day.format("DD")}</p>
             </header>
         </div>
     )
